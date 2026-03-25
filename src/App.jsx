5d655c43eb87f52e71cd5e10dfbb77a4dd0f65dd
Siva Priyanka <sivapriyankamedisetty@gmail.com>
2026-03-25 17:34:13 +0530

    Initial commit - Agentify app setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World. WIP for Agentify App. 🫡 </h1>
+      <h2>Coming Soon!🎉🎉🎉</h2>
+    </div>
+  );
+};
+
+export default App;
